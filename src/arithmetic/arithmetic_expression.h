2e5776bfd8b312381756e029efa6d0ac6fe139b8
Jeffrey Lovitz <jlovitz@emcien.com>
2018-04-01 13:42:24 -0400

    Fixed arithmetic_expression include paths

--- src/arithmetic/arithmetic_expression.h
+++ src/arithmetic/arithmetic_expression.h
@@ -1,10 +1,10 @@
 #ifndef __ARITHMETIC_EXPRESSION_H
 #define __ARITHMETIC_EXPRESSION_H
 
-#include "./graph/graph_entity.h"
-#include "./arithmetic/agg_ctx.h"
-#include "./parser/ast.h"
-#include "./graph/graph.h"
+#include "../graph/graph_entity.h"
+#include "agg_ctx.h"
+#include "../parser/ast.h"
+#include "../graph/graph.h"
 
 /* Forward declarations. */
 struct AR_ExpNode;
