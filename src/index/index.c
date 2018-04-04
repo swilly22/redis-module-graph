275e6c018f6605b21b262a2986d8ac8fe98ef785
Jeffrey Lovitz <jlovitz@emcien.com>
2018-04-04 17:10:10 -0400

    Validate type of SIValue keys in indices

--- src/index/index.c
+++ src/index/index.c
@@ -30,7 +30,7 @@ int compareSI(void *p1, void *p2, void *ctx) {
   }
 
   // We can only compare string and numeric SIValues, so this point
-  // should be unreachable for a properly constructed index. 
+  // should be unreachable for a properly constructed index.
 
   return 0;
 }
@@ -40,8 +40,8 @@ void freeVal(void *p1) {
 }
 
 skiplist* createIndex(RedisModuleCtx *ctx, const char *graphName, AST_IndexOpNode *indexOp) {
-  const char *label = indexOp->label;
-  const char *index_prop = indexOp->property;
+  const char *label = indexOp->target.label;
+  const char *index_prop = indexOp->target.property;
 
   LabelStore *store = LabelStore_Get(ctx, STORE_NODE, graphName, label);
 
@@ -54,6 +54,9 @@ skiplist* createIndex(RedisModuleCtx *ctx, const char *graphName, AST_IndexOpNod
   Node *node;
   EntityProperty *prop;
 
+  SIType lastKeyType = SI_NUMERIC | T_STRING;
+  SIType xor;
+
   while(LabelStoreIterator_Next(it_test, &nodeId, &nodeIdLen, (void**)&node)) {
     for (int i = 0; i < node->prop_count; i ++) {
       prop = node->properties + i;
@@ -61,12 +64,26 @@ skiplist* createIndex(RedisModuleCtx *ctx, const char *graphName, AST_IndexOpNod
       if (!strcmp(index_prop, prop->name)) {
         SIValue *key = malloc(sizeof(SIValue));
         *key = SI_Clone(prop->value);
+
+        xor = key->type ^ lastKeyType;
+        if (!xor) {
+          // Exact match to last key type
+        } else if (xor <= SI_NUMERIC) {
+          // if ==, first comparison made and key type was string
+          // if <, value was different non-numeric type or first comparison
+          lastKeyType = key->type;
+        } else {
+          // TODO We will currently skip matching label-property pairs of incorrect property types
+          continue;
+        }
+
         skiplistInsert(sl, key, node);
         break;
       }
     }
   }
 
+  /*
   skiplistIterator iter = skiplistIterateAll(sl);
   while ((node = skiplistIterator_Next(&iter)) != NULL) {
     printf("LABEL: %s\n", node->label);
@@ -77,6 +94,14 @@ skiplist* createIndex(RedisModuleCtx *ctx, const char *graphName, AST_IndexOpNod
       printf("\n");
     }
   }
+  */
 
   return sl;
+}
+
+skiplist* findIndex(RedisModuleCtx *ctx, const char *graphName, IndexTarget *target) {
+  // TODO Somewhere indices need to be stored, and can then be looked up by label-property pairs
+  // IndexStore?
+
+  return NULL;
 }
